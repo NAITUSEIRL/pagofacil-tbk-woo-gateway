e5b2858afc2d8f15347ad5174c029bc3e243dae6
Cristian Tala Sanchez <yomismo@cristiantala.cl>
2016-06-13 21:33:36 -0400

    Probando.

--- classes/WC_Gateway_TBKAAS.php
+++ classes/WC_Gateway_TBKAAS.php
@@ -93,11 +93,14 @@ class WC_Gateway_TBKAAS extends \WC_Payment_Gateway {
         $this->init_settings();
         // Define user set variables
         $this->title = $this->get_option('title');
-//        $this->description = $this->getDescription();
-//        $this->notification_url = str_replace('https:', 'http:', add_query_arg('wc-api', 'WooPagosMP', home_url('/')));
+        $this->description = $this->get_option('description');
+
+
         add_action('woocommerce_receipt_' . $this->id, array($this, 'receipt_page'));
         add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
-        add_action('woocommerce_api_wc_gateway_paypal', array($this, 'check_ipn_response'));
+         
+        //Payment listener/API hook
+        add_action('woocommerce_api_wc_gateway_' . $this->id, array($this, 'tbkaas_api_handler'));
     }
 
     function init_form_fields() {
