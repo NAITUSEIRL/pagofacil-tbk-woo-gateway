2ba5ccee372485193b118214aec59bc4a5346ef4
Cristian Tala S. <cristian.tala@gmail.com>
2016-08-25 18:13:45 -0300

    Agregando Versión

--- tbkaas-woo-gateway.php
+++ tbkaas-woo-gateway.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
-  Plugin Name: tbkaas-woo-gateway
+  Plugin Name: WebpayPlus PST para Woocommerce
   Plugin URI:  https://github.com/ctala/tbkaas-woo-gateway
-  Description: Pasarela de Pagos para Woocommerce y Transbank usando WebPayPlus Webservices a través de TBKAAS
-  Version:     0.1-PROD
+  Description: Pasarela de Pagos para Woocommerce y Transbank usando WebPayPlus Webservices a través de As A Service Labs
+  Version:     V1.1
   Author:      Cristian Tala Sánchez
   Author URI:  http://www.cristiantala.cl
   License:     MIT
@@ -15,7 +15,6 @@
 
 include_once 'vendor/autoload.php';
 
-//use tbkaaswoogateway\classes\Logger;
 use tbkaaswoogateway\classes\WC_Gateway_TBKAAS;
 
 //SERVIDORES
@@ -23,7 +22,7 @@ define("SERVER_DESARROLLO", "https://dev-env.sv1.tbk.cristiantala.cl/tbk/v2/");
 define("SERVER_PRODUCCION", "https://sv1.tbk.cristiantala.cl/tbk/v2/");
 
 //CONSTANTES
-//define("SERVER_BASE", "https://sv1.tbk.cristiantala.cl/tbk/v2/");
+
 define("SERVER_TBKAAS", "initTransaction");
 define("SERVER_TBKAAS_VERIFICAR", "estadoOrden");
 define("SERVER_TBKAAS_DETALLE", "getOrden");
@@ -32,20 +31,17 @@ define("SERVER_TBKAAS_DETALLE", "getOrden");
 //Funciones
 add_action('plugins_loaded', 'init_TBKAAS');
 
-function init_TBKAAS()
-{
+function init_TBKAAS() {
     if (!class_exists('WC_Payment_Gateway'))
         return;
 
-    class WC_Gateway_TBKAAS_Chile extends WC_Gateway_TBKAAS
-    {
+    class WC_Gateway_TBKAAS_Chile extends WC_Gateway_TBKAAS {
         
     }
 
 }
 
-function add_your_gateway_class($methods)
-{
+function add_your_gateway_class($methods) {
     $methods[] = 'WC_Gateway_TBKAAS_Chile';
     return $methods;
 }
